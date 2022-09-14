a137caedb2a90198aca9ca071d23632cf278656f
Andrew Constancio <61609406+andrewconstancio@users.noreply.github.com>
2022-09-14 11:13:16 -0500

    testing

--- UsersNeetBook/ContentView.swift
+++ UsersNeetBook/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                     CurrentlyReadingView()
                 }
                 .tabItem {
-                    Text("Reading")
+                    Text("Reading!!!!")
                 }
                 NavigationView {
                     WantToReadView()
